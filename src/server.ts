adc36f91eb830cc6e84ce0af7f7ce2699ea28058
KPMGE <jesuskevin254@gmail.com>
2021-10-03 12:03:02 -0300

    Adicionando banco de dados sqlite3 e migrations

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,27 @@
-import express from "express";
+import "reflect-metadata";
+import express, { Request, Response, NextFunction } from "express";
+import "./database";
+import "express-async-errors";
+import { router } from "./routes";
 
 const app = express();
+app.use(express.json());
 
-app.post("/post", (request, response) => {
-  return response.send("TEst post");
-});
+app.use(router);
 
-app.get("/test", (request, response) => {
-  return response.send("Hello World!");
-});
+app.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof Error) {
+      return response.status(400).json({
+        error: error.message,
+      });
+    }
+
+    return response.status(500).json({
+      status: "error",
+      message: "Internal Server Eror",
+    });
+  }
+);
 
 app.listen(3000, () => console.log("Server running at port 3000..."));
